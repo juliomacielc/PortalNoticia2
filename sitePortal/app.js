9f8208d18fc8c3d31407d0e0e46a47769921e04a
Julio Maciel Coelho <juliomacielcoelho@gmail.com>
2020-06-15 21:32:13 -0300

    ...

--- sitePortal/app.js
+++ sitePortal/app.js
@@ -1,1 +1,8 @@
-onload = alert('Hop')
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=4d06854944e94e18997725ab852d67d7';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
